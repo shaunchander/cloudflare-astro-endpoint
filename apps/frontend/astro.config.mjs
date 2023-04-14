21a997e8aa6e19b40ec78e7391220a5599ec8abe
shaunchander <shaunchander@gmail.com>
2023-04-14 11:39:14 -0500

    ♻️ update

--- apps/frontend/astro.config.mjs
+++ apps/frontend/astro.config.mjs
@@ -5,7 +5,5 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
 	output: "server",
-	adapter: cloudflare({
-		mode: "directory",
-	}),
+	adapter: cloudflare(),
 });
